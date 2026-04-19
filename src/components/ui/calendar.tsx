5dd977f480499f594625c151b5dfecde39e682db
Matteo Bernardini <matteobern92@gmail.com>
2026-04-19 18:41:40 +0200

    chore(deps): upgrade date-fns and react-day-picker

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -42,8 +42,17 @@ function Calendar({ className, classNames, showOutsideDays = true, ...props }: C
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ..._props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ..._props }) => <ChevronRight className="h-4 w-4" />,
+        Chevron: ({ orientation, className, ...props }) => {
+          if (orientation === "left") {
+            return <ChevronLeft className={cn("h-4 w-4", className)} {...props} />;
+          }
+
+          if (orientation === "right") {
+            return <ChevronRight className={cn("h-4 w-4", className)} {...props} />;
+          }
+
+          return null;
+        },
       }}
       {...props}
     />
